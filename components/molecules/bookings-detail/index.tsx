fdda6af8ca61d6ccb9b4a7e7f29f7e7b2aa8644b
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-01-12 12:02:10 +0800

    Update index.tsx

--- components/molecules/bookings-detail/index.tsx
+++ components/molecules/bookings-detail/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {StyledSafeAreaView} from '../../../styles/container';
 
 import HomeHeader from '../../atoms/home-header';
+import {Dimensions, ScrollView} from 'react-native';
 
 // @ts-ignore
 function BookingsDetail({navigation}) {
@@ -17,7 +18,19 @@ function BookingsDetail({navigation}) {
         title={'Booking Detail'}
         main={false}
       />
-      <></>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+        }}
+        style={{
+          width: '100%',
+          height: Dimensions.get('window').height * 0.9,
+          backgroundColor: '#e7e7e7',
+        }}>
+        <></>
+      </ScrollView>
     </StyledSafeAreaView>
   );
 }
