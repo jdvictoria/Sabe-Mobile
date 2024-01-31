b1dcd9856e8da87c75bf5097a2870fcd043cb0d7
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-01-31 15:43:18 +0800

    Finalize Driver Profile (Frontend)

--- components/molecules/driver-profile/index.tsx
+++ components/molecules/driver-profile/index.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import {Dimensions, ScrollView} from 'react-native';
 
-import {StyledSafeAreaView} from '../../../styles/container';
+import {
+  StyledCol,
+  StyledRow,
+  StyledSafeAreaView,
+  StyledTouchableCol,
+} from '../../../styles/container';
+import {styledText, StyledText14, StyledText18} from '../../../styles/text';
+
+// @ts-ignore
+import Rating from '../../../assets/icons/rating.svg';
 
 import HomeHeader from '../../atoms/home-header';
+import ButtonSettings from '../../atoms/button-settings';
 
 // @ts-ignore
 function DriverProfile({navigation}) {
+  const sans = styledText();
+
   return (
     <StyledSafeAreaView
       style={{
-        backgroundColor: '#f3f3f3',
+        justifyContent: 'flex-start',
+        backgroundColor: '#042F40',
       }}>
       <HomeHeader
         navigation={navigation}
@@ -28,10 +41,52 @@ function DriverProfile({navigation}) {
           position: 'absolute',
           bottom: 0,
           width: '100%',
-          height: Dimensions.get('window').height * 0.9,
+          height: Dimensions.get('window').height * 0.89,
           backgroundColor: '#e7e7e7',
+          borderTopLeftRadius: 50,
+          borderTopRightRadius: 50,
         }}
-      />
+        scrollEnabled={false}>
+        <StyledCol style={{width: '100%', marginTop: 25}}>
+          <StyledCol>
+            <StyledTouchableCol
+              style={{
+                width: 100,
+                height: 100,
+                backgroundColor: '#fff',
+                borderRadius: 50,
+                shadowColor: '#000',
+                shadowOffset: {width: 0, height: 2},
+                shadowOpacity: 0.2,
+                shadowRadius: 4,
+              }}
+            />
+            <StyledText18
+              style={[sans.bold, {color: '#042F40', marginTop: 10}]}>
+              Joshua Arlo Victoria
+            </StyledText18>
+            <StyledText14 style={[sans.bold, {color: '#042F40', marginTop: 5}]}>
+              09556736262 | vjoshuaarlo12@gmail.com
+            </StyledText14>
+            <StyledRow style={{marginTop: 10}}>
+              <Rating width={30} height={30} />
+              <StyledText18
+                style={[
+                  sans.bold,
+                  {color: '#042F40', marginLeft: 5, marginTop: 1.5},
+                ]}>
+                4.5
+              </StyledText18>
+            </StyledRow>
+          </StyledCol>
+          <StyledCol style={{width: '100%', marginTop: 10}}>
+            <ButtonSettings setting={'Frequently Asked Questions'} />
+            <ButtonSettings setting={'Share To Friends'} />
+            <ButtonSettings setting={'Contact Support'} />
+            <ButtonSettings setting={'About Us'} />
+          </StyledCol>
+        </StyledCol>
+      </ScrollView>
     </StyledSafeAreaView>
   );
 }
