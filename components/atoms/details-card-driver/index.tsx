0ef2db221875effb3af4456c6fb946da799f5688
Joshua Victoria <jdvictoria.tech@gmail.com>
2024-02-13 20:19:26 +0800

    Fix View

--- components/atoms/details-card-driver/index.tsx
+++ components/atoms/details-card-driver/index.tsx
@@ -51,7 +51,6 @@ function DetailsCardDriver({id, data, onApprove}: any) {
       {showID && (
         <StyledTouchableCol
           style={{
-            position: 'absolute',
             justifyContent: 'flex-start',
             width: '100%',
             height: Dimensions.get('window').height,
@@ -63,7 +62,7 @@ function DetailsCardDriver({id, data, onApprove}: any) {
               position: 'absolute',
               justifyContent: 'flex-start',
               width: '100%',
-              height: Dimensions.get('window').height,
+              height: Dimensions.get('window').height * 1,
               backgroundColor: 'gray',
               opacity: 0.5,
             }}
@@ -105,231 +104,233 @@ function DetailsCardDriver({id, data, onApprove}: any) {
         </StyledTouchableCol>
       )}
 
-      <StyledCol
-        style={{
-          justifyContent: 'space-between',
-          width: '85%',
-          height: 275,
-          marginTop: 25,
-          marginBottom: 5,
-          paddingTop: 12.5,
-          paddingBottom: 12.5,
-          backgroundColor: '#fff',
-          borderRadius: 10,
-        }}>
-        <StyledRow
+      {!showID && (
+        <StyledCol
           style={{
-            width: '87.5%',
             justifyContent: 'space-between',
+            width: '85%',
+            height: 275,
+            marginTop: 25,
+            marginBottom: 5,
+            paddingTop: 12.5,
+            paddingBottom: 12.5,
+            backgroundColor: '#fff',
+            borderRadius: 10,
           }}>
-          <SabeLogo width={25} height={25} />
-          <StyledTouchableRow onPress={handleShowID}>
-            <StyledText14 style={[sans.bold, {color: '#042F40'}]}>
-              View ID/s
-            </StyledText14>
-            <ArrowRight width={25} height={25} />
-          </StyledTouchableRow>
-        </StyledRow>
-
-        <StyledCol style={{width: '90%'}}>
-          <StyledText14
-            style={[
-              sans.regular,
-              {
-                color: '#1FBF83',
-                alignSelf: 'flex-start',
-              },
-            ]}>
-            Email
-          </StyledText14>
-          <StyledText16
-            style={[
-              sans.bold,
-              {
-                color: '#042F40',
-                alignSelf: 'flex-start',
-              },
-            ]}>
-            {data.email}
-          </StyledText16>
-        </StyledCol>
-
-        <StyledRow style={{width: '90%'}}>
-          <StyledCol style={{width: '60%'}}>
-            <StyledText14
-              style={[
-                sans.regular,
-                {
-                  color: '#1FBF83',
-                  alignSelf: 'flex-start',
-                },
-              ]}>
-              Name
-            </StyledText14>
-            <StyledText16
-              style={[
-                sans.bold,
-                {
-                  color: '#042F40',
-                  alignSelf: 'flex-start',
-                },
-              ]}>
-              {data.name}
-            </StyledText16>
-          </StyledCol>
-          <StyledCol style={{width: '40%'}}>
-            <StyledText14
-              style={[
-                sans.regular,
-                {
-                  color: '#1FBF83',
-                  alignSelf: 'flex-start',
-                },
-              ]}>
-              Phone
-            </StyledText14>
-            <StyledText16
-              style={[
-                sans.bold,
-                {
-                  color: '#042F40',
-                  alignSelf: 'flex-start',
-                },
-              ]}>
-              {data.contact}
-            </StyledText16>
-          </StyledCol>
-        </StyledRow>
-
-        <StyledRow style={{width: '90%'}}>
-          <StyledCol style={{width: '60%'}}>
-            <StyledText14
-              style={[
-                sans.regular,
-                {
-                  color: '#1FBF83',
-                  alignSelf: 'flex-start',
-                },
-              ]}>
-              Car Make
-            </StyledText14>
-            <StyledText16
-              style={[
-                sans.bold,
-                {
-                  color: '#042F40',
-                  alignSelf: 'flex-start',
-                },
-              ]}>
-              {data.carMake}
-            </StyledText16>
-          </StyledCol>
-          <StyledCol style={{width: '40%'}}>
-            <StyledText14
-              style={[
-                sans.regular,
-                {
-                  color: '#1FBF83',
-                  alignSelf: 'flex-start',
-                },
-              ]}>
-              Car Series
-            </StyledText14>
-            <StyledText16
-              style={[
-                sans.bold,
-                {
-                  color: '#042F40',
-                  alignSelf: 'flex-start',
-                },
-              ]}>
-              {data.carSeries}
-            </StyledText16>
-          </StyledCol>
-        </StyledRow>
-
-        <StyledRow style={{width: '90%'}}>
-          <StyledCol style={{width: '60%'}}>
-            <StyledText14
-              style={[
-                sans.regular,
-                {
-                  color: '#1FBF83',
-                  alignSelf: 'flex-start',
-                },
-              ]}>
-              Car Color
-            </StyledText14>
-            <StyledText16
-              style={[
-                sans.bold,
-                {
-                  color: '#042F40',
-                  alignSelf: 'flex-start',
-                },
-              ]}>
-              {data.carColor}
-            </StyledText16>
-          </StyledCol>
-          <StyledCol style={{width: '40%'}}>
-            <StyledText14
-              style={[
-                sans.regular,
-                {
-                  color: '#1FBF83',
-                  alignSelf: 'flex-start',
-                },
-              ]}>
-              Plate Number
-            </StyledText14>
-            <StyledText16
-              style={[
-                sans.bold,
-                {
-                  color: '#042F40',
-                  alignSelf: 'flex-start',
-                },
-              ]}>
-              {data.carPlate}
-            </StyledText16>
-          </StyledCol>
-        </StyledRow>
-
-        <StyledRow
-          style={{
-            width: '90%',
-            justifyContent: 'space-between',
-            alignItems: 'flex-end',
-          }}>
-          <StyledCol>
-            <StyledText14
-              style={[
-                sans.regular,
-                {
-                  color: '#1FBF83',
-                  alignSelf: 'flex-start',
-                },
-              ]}>
-              Type
-            </StyledText14>
-            <StyledText16
-              style={[
-                sans.bold,
-                {
-                  color: '#042F40',
-                  alignSelf: 'flex-start',
-                },
-              ]}>
-              {data.type.toUpperCase()}
-            </StyledText16>
-          </StyledCol>
-          <StyledRow>
-            <StyledTouchableRow style={{marginLeft: 5}} onPress={handleApprove}>
-              <Check width={25} height={25} />
+          <StyledRow
+            style={{
+              width: '87.5%',
+              justifyContent: 'space-between',
+            }}>
+            <SabeLogo width={25} height={25} />
+            <StyledTouchableRow onPress={handleShowID}>
+              <StyledText14 style={[sans.bold, {color: '#042F40'}]}>
+                View ID/s
+              </StyledText14>
+              <ArrowRight width={25} height={25} />
             </StyledTouchableRow>
           </StyledRow>
-        </StyledRow>
-      </StyledCol>
+
+          <StyledCol style={{width: '90%'}}>
+            <StyledText14
+              style={[
+                sans.regular,
+                {
+                  color: '#1FBF83',
+                  alignSelf: 'flex-start',
+                },
+              ]}>
+              Email
+            </StyledText14>
+            <StyledText16
+              style={[
+                sans.bold,
+                {
+                  color: '#042F40',
+                  alignSelf: 'flex-start',
+                },
+              ]}>
+              {data.email}
+            </StyledText16>
+          </StyledCol>
+
+          <StyledRow style={{width: '90%'}}>
+            <StyledCol style={{width: '60%'}}>
+              <StyledText14
+                style={[
+                  sans.regular,
+                  {
+                    color: '#1FBF83',
+                    alignSelf: 'flex-start',
+                  },
+                ]}>
+                Name
+              </StyledText14>
+              <StyledText16
+                style={[
+                  sans.bold,
+                  {
+                    color: '#042F40',
+                    alignSelf: 'flex-start',
+                  },
+                ]}>
+                {data.name}
+              </StyledText16>
+            </StyledCol>
+            <StyledCol style={{width: '40%'}}>
+              <StyledText14
+                style={[
+                  sans.regular,
+                  {
+                    color: '#1FBF83',
+                    alignSelf: 'flex-start',
+                  },
+                ]}>
+                Phone
+              </StyledText14>
+              <StyledText16
+                style={[
+                  sans.bold,
+                  {
+                    color: '#042F40',
+                    alignSelf: 'flex-start',
+                  },
+                ]}>
+                {data.contact}
+              </StyledText16>
+            </StyledCol>
+          </StyledRow>
+
+          <StyledRow style={{width: '90%'}}>
+            <StyledCol style={{width: '60%'}}>
+              <StyledText14
+                style={[
+                  sans.regular,
+                  {
+                    color: '#1FBF83',
+                    alignSelf: 'flex-start',
+                  },
+                ]}>
+                Car Make
+              </StyledText14>
+              <StyledText16
+                style={[
+                  sans.bold,
+                  {
+                    color: '#042F40',
+                    alignSelf: 'flex-start',
+                  },
+                ]}>
+                {data.carMake}
+              </StyledText16>
+            </StyledCol>
+            <StyledCol style={{width: '40%'}}>
+              <StyledText14
+                style={[
+                  sans.regular,
+                  {
+                    color: '#1FBF83',
+                    alignSelf: 'flex-start',
+                  },
+                ]}>
+                Car Series
+              </StyledText14>
+              <StyledText16
+                style={[
+                  sans.bold,
+                  {
+                    color: '#042F40',
+                    alignSelf: 'flex-start',
+                  },
+                ]}>
+                {data.carSeries}
+              </StyledText16>
+            </StyledCol>
+          </StyledRow>
+
+          <StyledRow style={{width: '90%'}}>
+            <StyledCol style={{width: '60%'}}>
+              <StyledText14
+                style={[
+                  sans.regular,
+                  {
+                    color: '#1FBF83',
+                    alignSelf: 'flex-start',
+                  },
+                ]}>
+                Car Color
+              </StyledText14>
+              <StyledText16
+                style={[
+                  sans.bold,
+                  {
+                    color: '#042F40',
+                    alignSelf: 'flex-start',
+                  },
+                ]}>
+                {data.carColor}
+              </StyledText16>
+            </StyledCol>
+            <StyledCol style={{width: '40%'}}>
+              <StyledText14
+                style={[
+                  sans.regular,
+                  {
+                    color: '#1FBF83',
+                    alignSelf: 'flex-start',
+                  },
+                ]}>
+                Plate Number
+              </StyledText14>
+              <StyledText16
+                style={[
+                  sans.bold,
+                  {
+                    color: '#042F40',
+                    alignSelf: 'flex-start',
+                  },
+                ]}>
+                {data.carPlate}
+              </StyledText16>
+            </StyledCol>
+          </StyledRow>
+
+          <StyledRow
+            style={{
+              width: '90%',
+              justifyContent: 'space-between',
+              alignItems: 'flex-end',
+            }}>
+            <StyledCol>
+              <StyledText14
+                style={[
+                  sans.regular,
+                  {
+                    color: '#1FBF83',
+                    alignSelf: 'flex-start',
+                  },
+                ]}>
+                Type
+              </StyledText14>
+              <StyledText16
+                style={[
+                  sans.bold,
+                  {
+                    color: '#042F40',
+                    alignSelf: 'flex-start',
+                  },
+                ]}>
+                {data.type.toUpperCase()}
+              </StyledText16>
+            </StyledCol>
+            <StyledRow>
+              <StyledTouchableRow style={{marginLeft: 5}} onPress={handleApprove}>
+                <Check width={25} height={25} />
+              </StyledTouchableRow>
+            </StyledRow>
+          </StyledRow>
+        </StyledCol>
+      )}
     </>
   );
 }
